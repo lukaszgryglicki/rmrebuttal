a5b261873afe5854103ef04adf8a4e78ef99623e
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-12 10:04:09 +0000

    Added containerd support
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- example_merges.sh
+++ example_merges.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-PROJS="prometheus kubernetes allprj" ./merge_results.sh
-RRE="release;;;(?im)cncf" RNAME="join" PROJS="prometheus kubernetes allprj" ./merge_results.sh
+PROJS="kubernetes prometheus containerd allprj" ./merge_results.sh
+RRE="release;;;(?im)cncf" RNAME="join" PROJS="prometheus kubernetes containerd allprj" ./merge_results.sh
 RRE='release;;;^\d{4}$' RNAME="yearly" PROJS="allprj" ./merge_results.sh
-RRE='release;;;^Quarter from' RNAME="quarterly" PROJS="allprj kubernetes prometheus" ./merge_results.sh
-RRE='release;;;^\d{4}-\d{2}-\d{2}$' RNAME="monthly" PROJS="allprj kubernetes prometheus" ./merge_results.sh
-RRE='release;;;(?i)v\d\.\d' RNAME="releases" PROJS="kubernetes prometheus" ./merge_results.sh
+RRE='release;;;^Quarter from' RNAME="quarterly" PROJS="kubernetes prometheus containerd allprj" ./merge_results.sh
+RRE='release;;;^\d{4}-\d{2}-\d{2}$' RNAME="monthly" PROJS="kubernetes prometheus containerd allprj" ./merge_results.sh
+RRE='release;;;(?i)v\d\.\d' RNAME="releases" PROJS="kubernetes prometheus containerd" ./merge_results.sh
