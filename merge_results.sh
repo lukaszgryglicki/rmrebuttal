ea97aa85ad0fc92235de25b8ae51347b066b12e2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2018-04-23 08:37:20 +0000

    Needed data for KubCon.EU K8s report
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- merge_results.sh
+++ merge_results.sh
@@ -16,7 +16,7 @@ then
 fi
 if [ -z "$COLFMT" ]
 then
-  COLFMT="release,Release,,;date_from,Date from,d,1/2/2006;date_to,Date to,d,1/2/2006;n_top_contributing_coms,Distinct companies for top %s contributors,,;n_top_committing_coms,Distinct companies for top %s committers,,;top_commits_perc,Top %s developers commits as percent of all,n,%.1f%%;top_contributions_perc,Top %s contributors contributions as percent of all,n,%.1f%%;top_comp_contributions_perc,Top %s companies contributions as percent of all,n,%.1f%%;top_comp_commits_perc,Top %s companies commits as percent of all,n,%.1f%%"
+  COLFMT="release,Release,,;date_from,Date from,d,1/2/2006;date_to,Date to,d,1/2/2006;n_top_contributing_coms,Distinct companies for top %s contributors,,;n_top_committing_coms,Distinct companies for top %s committers,,;top_commits_perc,Top %s developers commits as percent of all,n,%.1f%%;top_contributions_perc,Top %s contributors contributions as percent of all,n,%.1f%%;top_comp_contributions_perc,Top %s companies contributions as percent of all,n,%.1f%%;top_comp_commits_perc,Top %s companies commits as percent of all,n,%.1f%%;top_contributing_coms,Top %s contributing companies,,;top_committing_coms,Top %s committing companies,,"
 fi
 export COLFMT
 for proj in $PROJS
@@ -37,5 +37,7 @@ do
   PG_DB=$db ./rmrebuttal 3 'release;date_from;date_to;top_commits_perc' '1;2;3;4;5;10;15;20;25;30;50;100;200;500;1000' "$RRE" "${pref}_percent_commits.csv" || exit 6
   PG_DB=$db ./rmrebuttal 3 'release;date_from;date_to;top_comp_contributions_perc' '1;2;3;4;5;10;15;20;25;30;50;100;200;500;1000' "$RRE" "${pref}_comp_percent_contributions.csv" || exit 5
   PG_DB=$db ./rmrebuttal 3 'release;date_from;date_to;top_comp_commits_perc' '1;2;3;4;5;10;15;20;25;30;50;100;200;500;1000' "$RRE" "${pref}_comp_percent_commits.csv" || exit 6
+  PG_DB=$db ./rmrebuttal 3 'release;date_from;date_to;top_contributing_coms' '1;2;3;4;5;10;15;20;25;30;50;100;200;500;1000' "$RRE" "${pref}_contributing_comp_names.csv" || exit 7
+  PG_DB=$db ./rmrebuttal 3 'release;date_from;date_to;top_committing_coms' '1;2;3;4;5;10;15;20;25;30;50;100;200;500;1000' "$RRE" "${pref}_committing_comp_names.csv" || exit 7
 done
 echo 'OK'
