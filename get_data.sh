f0dd1eeeed667a536c738fb05bb14e7ab1cb8be6
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-05 06:03:07 +0000

    Updates from generating data
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- get_data.sh
+++ get_data.sh
@@ -15,4 +15,4 @@ then
 else
   pref=$PG_DB
 fi
-GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 GHA2DB_CSVOUT="results/${pref}_top_$1.csv" ./runq ./query.sql {{exclude_bots}} "`cat ./exclude_bots.sql`" {{n}} "$1" {{start_date}} `cat ./start_dates/${pref}.txt` {{join_date}} `cat ./join_dates/${pref}.txt` {{proj_rels}} "readfile:./partials/${pref}_rels.sql"
+GHA2DB_SKIPTIME=1 GHA2DB_SKIPLOG=1 GHA2DB_CSVOUT="results/${pref}_top_$1.csv" runq ./query.sql {{exclude_bots}} "`cat ./exclude_bots.sql`" {{n}} "$1" {{start_date}} `cat ./start_dates/${pref}.txt` {{join_date}} `cat ./join_dates/${pref}.txt` {{proj_rels}} "readfile:./partials/${pref}_rels.sql"
