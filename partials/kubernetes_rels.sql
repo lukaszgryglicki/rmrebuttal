73857f3f35b7afbbdbb9a064c552b508e0ea0477
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-05 08:00:38 +0000

    Update K8s releases
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- partials/kubernetes_rels.sql
+++ partials/kubernetes_rels.sql
@@ -9,9 +9,14 @@ union select 1008 as ord, '2017-03-28 16:23:06'::timestamp as f, '2017-06-29 22:
 union select 1009 as ord, '2017-06-29 22:53:16'::timestamp as f, '2017-09-28 22:13:57'::timestamp as t, 'v1.7.0 - v1.8.0' as rel
 union select 1010 as ord, '2017-09-28 22:13:57'::timestamp as f, '2017-12-15 20:53:13'::timestamp as t, 'v1.8.0 - v1.9.0' as rel
 union select 1011 as ord, '2017-12-15 20:53:13'::timestamp as f, '2018-03-26 16:41:58'::timestamp as t, 'v1.9.0 - v1.10.0' as rel
-union select 1012 as ord, '2018-03-26 16:41:58'::timestamp as f, now()::date as t, 'v1.10.0 - now' as rel
+union select 1012 as ord, '2018-03-26 16:41:58'::timestamp as f, '2018-06-27 20:06:28'::timestamp as t, 'v1.10.0 - v1.11.0' as rel
+union select 1013 as ord, '2018-06-27 20:06:28'::timestamp as f, '2018-09-27 16:54:27'::timestamp as t, 'v1.11.0 - v1.12.0' as rel
+union select 1014 as ord, '2018-09-27 16:54:27'::timestamp as f, '2018-12-03 20:54:59'::timestamp as t, 'v1.12.0 - v1.13.0' as rel
+union select 1015 as ord, '2018-12-03 20:54:59'::timestamp as f, '2019-03-25 15:44:16'::timestamp as t, 'v1.13.0 - v1.14.0' as rel
+union select 1016 as ord, '2019-03-25 15:44:16'::timestamp as f, now()::date as t, 'v1.14.0 - now' as rel
 union select 1101 as ord, (select timestamp from start_date) as f, '2015-01-01 00:00:00' as t, '2014' as rel
 union select 1103 as ord, '2015-01-01 00:00:00'::timestamp as f, '2016-01-01 00:00:00'::timestamp as t, '2015' as rel
 union select 1103 as ord, '2016-01-01 00:00:00'::timestamp as f, '2017-01-01 00:00:00'::timestamp as t, '2016' as rel
 union select 1104 as ord, '2017-01-01 00:00:00'::timestamp as f, '2018-01-01 00:00:00'::timestamp as t, '2017' as rel
-union select 1105 as ord, '2018-01-01 00:00:00'::timestamp as f, now()::date as t, '2018' as rel
+union select 1105 as ord, '2018-01-01 00:00:00'::timestamp as f, '2019-01-01 00:00:00'::timestamp as t, '2018' as rel
+union select 1106 as ord, '2019-01-01 00:00:00'::timestamp as f, now()::date as t, '2019' as rel
